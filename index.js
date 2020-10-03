16dd5037a973ef27061fba5720d44956c62716df
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-03 13:02:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const mongoose = require("mongoose");
 const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
