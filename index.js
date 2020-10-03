ffe28c69e483474b1501a77c5a25757bf4edbb4c
Name boy <53903319+namekinggamerboy@users.noreply.github.com>
2020-10-03 13:16:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
+app.locals.domain = "https://namekinggamerboy.github.io/uplife-server"; 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
